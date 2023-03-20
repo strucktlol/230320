f15dc9fa886604ca963db9a4b75806a130d85868
strucktlol <95130170+strucktlol@users.noreply.github.com>
2023-03-21 00:17:52 +0800

    Add files via upload

---
+++ indexActivity.js
@@ -0,0 +1,21 @@
+console.log("Hello, Welcome to De La Salle University!");
+
+let firstName = 'Jose Antonio';
+let lastName = 'Alcalde';
+let age = 18;
+let hobbies = ["Working Out", "Listening to Music", "Playing Online Games", "Watching Movies", "Reading Books"];
+let homeAddress = {
+	housenumber: "25-B",
+	street: 'Amethyst',
+	subdivision: 'Greenheights',
+	barangay: 'San Isidro',
+	city: 'Paranaque',
+	province: 'Metro Manila',
+	zipcode: 1700
+};
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(hobbies)
+console.log(homeAddress)
